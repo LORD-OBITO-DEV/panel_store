2d4f043bc387ef723e41abeb5c10e6ad4273bdb4
オビト・デヴ卿🐉🫟 <nguessanjeanstephane87@gmail.com>
2025-11-05 03:52:08 +0000

    Update server.js

--- server/server.js
+++ server/server.js
@@ -23,7 +23,7 @@ app.use(express.static(path.join(__dirname, '../client/build')));
 
 // Si aucune route API n'est matchée, renvoyer React
 app.get('*', (req, res) => {
-  res.sendFile(path.join(__dirname, '../client/build', 'index.html'));
+  res.sendFile(path.join(__dirname, '../client/', 'index.html'));
 });
 
 app.listen(PORT, () => {
