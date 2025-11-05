6ea5acea6ecbedc536fe39e00f1febf5281f9179
オビト・デヴ卿🐉🫟 <nguessanjeanstephane87@gmail.com>
2025-11-05 17:37:11 +0000

    Update panelRoutes.js

--- server/routes/panelRoutes.js
+++ server/routes/panelRoutes.js
@@ -1,7 +1,12 @@
 import express from "express";
-import { createPanel } from "../controllers/panelController.js";
+import { createPanelController, listPanelsController } from "../controllers/panelController.js";
 
 const router = express.Router();
-router.post("/create", createPanel);
+
+// create provisional panel (before payment) -> stores doc in Mongo as pending
+router.post("/create", createPanelController);
+
+// list panels (optionally by type)
+router.get("/list", listPanelsController);
 
 export default router;
